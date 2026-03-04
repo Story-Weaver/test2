0c1b3a410068f5ec810a51a9c8bb71fe3e4088e1
Roman <tkacevr64@gmail.com>
2026-03-05 01:53:39 +0300

    test process builder v2

--- src/main/java/by/storyweaver/back2/Service/HostService.java
+++ src/main/java/by/storyweaver/back2/Service/HostService.java
@@ -3,6 +3,8 @@ package by.storyweaver.back2.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
@@ -12,41 +14,59 @@ import by.storyweaver.back2.Something;
 @Service
 public class HostService {
 
-    public String createVM(Something entity) {
-    StringBuilder output = new StringBuilder();
+    public CompletableFuture<String> createVM(Something entity) {
+        return CompletableFuture.supplyAsync(() -> {
+            StringBuilder output = new StringBuilder();
+            
+        
+            ProcessBuilder pb = new ProcessBuilder("/home/user/git_clone/create.sh", 
+                entity.getName(), 
+                String.valueOf(entity.getCors()), 
+                String.valueOf(entity.getRam()), 
+                String.valueOf(entity.getRom()), 
+                entity.getPassword()
+            );
+            
+            pb.redirectErrorStream(true);
 
-    try {
-        String command = String.format("sudo bash ./create.sh %s %d %d %d %s",
-                entity.getName(), entity.getCors(), entity.getRam(), entity.getRom(), entity.getPassword());
+            try {
+                Process process = pb.start();
 
-        ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", command);
-        processBuilder.redirectErrorStream(true);
-        Process process = processBuilder.start();
+                // 2. Читаем поток вывода
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        output.append(line).append("\n");
+                    }
+                }
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
+                // 3. Ждем завершения с жестким таймаутом (например, 2 минуты)
+                boolean finished = process.waitFor(2, TimeUnit.MINUTES);
+
+                if (!finished) {
+                    process.destroyForcibly();
+                    return "Error: Script execution timeout (2 min exceeded)";
+                }
+
+                int exitCode = process.exitValue();
+                if (exitCode == 0) {
+                    return "Success:\n" + output.toString();
+                } else {
+                    return "Error Code: " + exitCode + "\nLog: " + output.toString();
+                }
+
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                return "Exception occurred: " + e.getMessage();
             }
-        }
-
-        int exitCode = process.waitFor();
-        if (exitCode == 0) {
-            return output.toString();
-        } else {
-            return "Error: " + exitCode + "\n" + output.toString();
-        }
-    } catch (IOException | InterruptedException e) {
-        e.printStackTrace();
-        return "Exception: " + e.getMessage();
-    }
+        });
     }
 
 
 
     public boolean startVM(String vmName, int sshPort) {
         try {
-            String command = String.format("bash /home/user/start.sh %s %d", vmName, sshPort);
+            String command = String.format("bash /home/user/git_clone/start.sh %s %d", vmName, sshPort);
             return executeBashScript(command);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -56,7 +76,7 @@ public class HostService {
 
     public boolean stopVM(String vmName) {
         try {
-            String command = String.format("bash /home/user/stop.sh %s", vmName);
+            String command = String.format("bash /home/user/git_clone/stop.sh %s", vmName);
             return executeBashScript(command);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -66,7 +86,7 @@ public class HostService {
 
     public boolean rebootVM(String vmName, int sshPort) {
         try {
-            String command = String.format("bash /path/to/reboot_vm.sh %s %d", vmName, sshPort);
+            String command = String.format("bash /home/user/git_clone/reboot.sh %s %d", vmName, sshPort);
             return executeBashScript(command);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -76,7 +96,7 @@ public class HostService {
 
     public boolean deleteVM(String vmName) {
         try {
-            String command = String.format("bash /path/to/delete_vm.sh %s", vmName);
+            String command = String.format("bash /home/user/git_clone/delete.sh %s", vmName);
             return executeBashScript(command);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -87,6 +107,6 @@ public class HostService {
     private boolean executeBashScript(String command) throws IOException, InterruptedException {
         Process process = Runtime.getRuntime().exec(command);
         int exitCode = process.waitFor();
-        return exitCode != 0;
+        return true;
     }
 }
